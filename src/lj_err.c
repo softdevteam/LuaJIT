cc3f42e54fcb834dd995ae7800ba033fbbecf255
Yichun Zhang (agentzh) <agentzh@gmail.com>
2014-05-12 15:25:08 -0700

    feature: save g->jit_base to g->saved_jit_base before lj_err_throw clears g->jit_base which makes it impossible to get Lua backtrace in such states.

--- src/lj_err.c
+++ src/lj_err.c
@@ -508,6 +508,7 @@ LJ_NOINLINE void LJ_FASTCALL lj_err_throw(lua_State *L, int errcode)
 {
   global_State *g = G(L);
   lj_trace_abort(g);
+  g->saved_jit_base = g->jit_base;
   setmref(g->jit_base, NULL);
   L->status = LUA_OK;
 #if LJ_UNWIND_EXT
