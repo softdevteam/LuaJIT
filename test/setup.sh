c5f651922c23cbbe820c1dbd4b483460b720bcc0
fsfod <fsfod11@gmail.com>
2017-11-03 21:17:05 +0000

    Use our fixed test suite branch

--- test/setup.sh
+++ test/setup.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR}
 
 if [ ! -d "testsuite" ]; then 
-  git clone https://github.com/softdevteam/LuaJIT-test-cleanup testsuite
+  git clone -b lib_apichanges https://github.com/softdevteam/LuaJIT-test-cleanup testsuite
 fi
