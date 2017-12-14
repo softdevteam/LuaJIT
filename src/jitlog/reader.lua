220c47db97dfd0b4d6875fc6cee71519ce79d3e4
fsfod <fsfod11@gmail.com>
2017-12-14 13:53:24 +0000

    Add a make_enum function to the reader and make defining enums less adhoc with it

--- src/jitlog/reader.lua
+++ src/jitlog/reader.lua
@@ -8,6 +8,29 @@ local MsgType = logdef.MsgType
 local msgnames = MsgType.names
 local msgsizes = logdef.msgsizes
 
+local enum_mt = {
+  __index = function(self, key)
+    if type(key) == "number" then
+      local result = self.names[key+1]
+      if not result then
+        error(format("No name exists for enum index %d max is %d", key, #self.names))
+      end
+      return result
+    end
+  end,
+  __len = function(self) return #self.names end
+}
+
+local function make_enum(names)
+  local t = setmetatable({}, enum_mt)
+  for i, name in ipairs(names) do
+    assert(name ~= "names")
+    t[name] = i-1
+  end
+  t.names = names
+  return t
+end
+
 local base_actions = {}
 
 function base_actions:stringmarker(msg)
@@ -29,7 +52,7 @@ end
 function base_actions:enumdef(msg)
   local name = msg:get_name()
   local names = msg:get_valuenames()
-  self.enums[name] = names
+  self.enums[name] = make_enum(names)
   self:log_msg("enumlist", "Enum(%s): %s", name, table.concat(names,","))
   return name, names
 end
@@ -260,14 +283,9 @@ function logreader:processheader(header)
   self.starttime = header.starttime
 
   -- Make the msgtype enum for this file
-  local msgtype = {
-    names = header.msgnames
-  } 
+  local msgtype = make_enum(header.msgnames)
   self.msgtype = msgtype
   self.msgnames = header.msgnames
-  for i, name in ipairs(header.msgnames) do
-    msgtype[name] = i-1
-  end
   
   for _, name in ipairs(msgnames) do
     if not msgtype[name] and name ~= "MAX" then
