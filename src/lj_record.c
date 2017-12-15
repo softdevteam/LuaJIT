a0b1cf6445f17b25d06067af03cfa9be5cd09173
fsfod <fsfod11@gmail.com>
2017-12-15 19:31:51 +0000

    Add per function hot counting
    Function hot counter have been moved from the shared hashtable to inside the
    function prototype object that is only shared between the closures of a
    function. The function hot counter field was added to the end of the GCproto
    struct so that its right next the function header bytecode in memory which
    decrements the counter.
    
    Since the function hot counters are now spread out around the function proto
    objects it will increase the number of dirty cache lines that need to
    be eventually be flushed back to memory vs only two cache lines of the shared
    hotcount hashtable, but this stops happening once the function is JIT'ed.
    
    Updated the interpreter assembly for the Lua function header bytecodes to use
    the new hot counter location.
    Added a separate JIT param for function hot trigger count currently set to the
    same effective value as it was with the shared hotcount table system of 112 calls.

--- src/lj_record.c
+++ src/lj_record.c
@@ -1667,9 +1667,11 @@ static void check_call_unroll(jit_State *J, TraceNo lnk)
   } else {
     if (count > J->param[JIT_P_callunroll]) {
       if (lnk) {  /* Possible tail- or up-recursion. */
+        GCproto *pt = (GCproto *)(((char *)J->pc)- sizeof(GCproto));
+        lua_assert(bc_op(*J->pc) == BC_JFUNCF);
 	lj_trace_flush(J, lnk);  /* Flush trace that only returns. */
 	/* Set a small, pseudo-random hotcount for a quick retry of JFUNC*. */
-	hotcount_set(J2GG(J), J->pc+1, LJ_PRNG_BITS(J, 4));
+        pt->hotcount = LJ_PRNG_BITS(J, 4);
       }
       lj_trace_err(J, LJ_TRERR_CUNROLL);
     }
