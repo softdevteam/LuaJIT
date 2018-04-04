5a5ec5e511ecce5988f94b3690a0288902a0bb3b
fsfod <fsfod11@gmail.com>
2018-04-04 23:03:20 +0100

    Fix hotcount_set not subbing -1 the count value  in lj_trace_hot

--- src/lj_trace.c
+++ src/lj_trace.c
@@ -736,7 +736,7 @@ void LJ_FASTCALL lj_trace_hot(jit_State *J, const BCIns *pc)
   /* Note: pc is the interpreter bytecode PC here. It's offset by 1. */
   ERRNO_SAVE
   /* Reset hotcount. */
-  hotcount_set(J2GG(J), pc, J->param[JIT_P_hotloop]*HOTCOUNT_LOOP);
+  hotcount_set(J2GG(J), pc, (J->param[JIT_P_hotloop]*HOTCOUNT_LOOP)-1);
   /* Only start a new trace if not recording or inside __gc call or vmevent. */
   if (J->state == LJ_TRACE_IDLE &&
       !(J2G(J)->hookmask & (HOOK_GC|HOOK_VMEVENT))) {
