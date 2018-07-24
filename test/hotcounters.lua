92b9645057a75190588e41691cef57ab710fda68
fsfod <fsfod11@gmail.com>
2018-07-24 04:51:37 +0100

    Refactor hotcounter tests to run without reading the internal counters

--- test/hotcounters.lua
+++ test/hotcounters.lua
@@ -13,8 +13,9 @@ local fhot = 56*2
 local lhot = 56
 local func_penalty = 36*2
 local loop_penalty = 36
-local maxattemps_func = 4
-local maxattemps_loop = 8
+local maxattemps_func = 10
+local maxattemps_loop = 11
+local random_backoff = 16
 
 local function getmaxcount(attempts, penalty)
   local count = penalty + 16
@@ -33,12 +34,59 @@ print("penaltymaxfunc="..countmax_func, "penaltymaxloop="..countmax_loop)
 
 jit.opt.start("penaltymaxfunc="..countmax_func, "penaltymaxloop="..countmax_loop)
 
-local function calln(f, n)
+local function calln(f, n, ...)
   for i=1, n do
-    f()
+    f(...)
   end
 end
 jit.off(calln)
+
+local function forcejoff(f, ...)
+  jit.off(f)
+  calln(f, 130, ...)
+  return
+end
+jit.off(forcejoff)
+
+local function nop() end
+jit.off(nop)
+
+local function force_nohotcount()
+  for i = 0, lhot+1 do
+    calln(nop, 1)
+  end
+  for i = 0, lhot+1 do
+    forcejoff(nop, 1)
+  end
+end
+jit.off(force_nohotcount)
+force_nohotcount()
+
+local tstarts, tstops, taborts = 0, 0, 0
+
+local function reset_tracestats()
+  tstarts, tstops, taborts = 0, 0, 0
+end
+force_nohotcount(reset_tracestats)
+
+local function trace_event(event)
+  if event == "start" then
+    tstarts = tstarts + 1
+  elseif event == "stop" then
+    tstops = tstops + 1
+  elseif event == "abort" then
+    taborts = taborts + 1 
+  end
+end
+
+jit.attach(trace_event, "trace")
+
+local function teststart()
+  reset_tracestats()
+  jit.off()
+  jit.on()
+end
+force_nohotcount(teststart)
 
 local countid_calls = -1
 local countid_loops = -2 -- total number of times a loop
@@ -58,91 +106,49 @@ end
 local tests = {}
 
 function tests.func_hotcounters()
+  teststart()
   local function f1() return 1 end
-  
-  assert(get_hotcount(f1, countid_func) == fhot)
-  assert(get_hotcount(f1, countid_calls) == 0)
-  assert(get_hotcount(f1, countid_loops) == 0)
-  
+
   f1()
-  assert(get_hotcount(f1, countid_func) == fhot-1)
-  assert(get_hotcount(f1, countid_calls) == 1) -- call count should of incremented
-  assert(get_hotcount(f1, countid_loops) == 0)
-  
-  f1(); f1(); f1(); f1();
-  assert(get_hotcount(f1, countid_func) == fhot-5)
-  assert(get_hotcount(f1, countid_calls) == 5)
-  assert(get_hotcount(f1, countid_loops) == 0)
-  
-  local function f2() end
-  calln(f2, fhot)
-  assert(get_hotcount(f2, countid_calls) == fhot, get_hotcount(f2, countid_calls))
-  assert(get_hotcount(f2, countid_func) == 0, get_hotcount(f2, countid_func) )
-  
-  jitlog.start()
-  -- Now trigger the trace
-  f2()
-  assert(get_hotcount(f2, countid_calls) == fhot+1, get_hotcount(f2, countid_calls))
-  assert(get_hotcount(f2, countid_func) == fhot, get_hotcount(f2, countid_func))
-  
-  -- Functinon JIT'ed now
-  f2()
-  -- Call counter still goes up for JFUNC
-  assert(get_hotcount(f2, countid_calls) == fhot+2, get_hotcount(f2, countid_calls))
-  -- But the function hot counter should stop changing now its JIT'ed
-  assert(get_hotcount(f2, countid_func) == fhot, get_hotcount(f2, countid_func))
-  
-  local result = parselog(jitlog.savetostring())
-  assert(#result.traces == 1)
-  assert(result.traces[1].callcount == fhot)
-  assert(result.traces[1].startpc == 0)
+  assert(tstarts == 0, tstarts)
+
+  calln(f1, fhot-1)
+  assert(tstarts == 0, tstarts)
+
+  -- Counter should be zero so this call triggers a trace
+  f1()
+  assert(tstarts == 1 and tstops == 1, tstarts)
+
+  -- Call it after its JIT'ed to make sure it won't its not creating another trace
+  calln(f1, fhot*3)
+  assert(tstarts == 1 and tstops == 1, tstarts)
 end
 
 function tests.loop_hotcounters()
+  teststart()
   local function f1(n) 
     a = 0 
     for i=1,n do a = a + 1 end
     return a
-  end 
-  assert(get_hotcount(f1, countid_func) == fhot)
-  assert(get_hotcount(f1, countid_loop1) == lhot)
-  assert(get_hotcount(f1, countid_calls) == 0)
-  assert(get_hotcount(f1, countid_loops) == 0)
+  end
   
-  f1(2)
-  assert(get_hotcount(f1, countid_func) == fhot-1)
-  assert(get_hotcount(f1, countid_loop1) == lhot-2)
-  assert(get_hotcount(f1, countid_calls) == 1)
-  assert(get_hotcount(f1, countid_loops) == 2)
+  f1(1)
+  assert(tstarts == 0, tstarts)
   
   -- Run the loop with an out of range count. no loop counts should change
   f1(-1)
-  assert(get_hotcount(f1, countid_loop1) == lhot-2)
-  assert(get_hotcount(f1, countid_loops) == 2)
-  assert(get_hotcount(f1, countid_func) == fhot-2)
-  --jitlog.reset()
-  jitlog.start()
+  assert(tstarts == 0, tstarts)
+
+  -- The loop hot counter should be zero after this call
+  f1(lhot-1)
+  assert(tstarts == 0, tstarts)
   
-  f1(lhot-2)
-  assert(get_hotcount(f1, countid_loop1) == 0)
-  
-  -- If the loop trip count is too low the loop trace will abort because the trace 
-  -- would have left the loop while still recording it
   f1(3)
-  -- Loop hotcounter is reset to the starting hot counter value when it starts a trace
-  assert(get_hotcount(f1, countid_loop1) == lhot, get_hotcount(f1, 1))
-  
-  f1(1)
-  assert(get_hotcount(f1, countid_loop1) == lhot)
-  assert(get_hotcount(f1, countid_loops) == lhot+1, get_hotcount(f1, countid_loops))
-  
-  local result = parselog(jitlog.savetostring())
-  assert(#result.traces == 1)
-  assert(result.traces[1].callcount == lhot, result.traces[1].callcount)
-  assert(result.traces[1].startpc ~= 0)
+  assert(tstarts == 1 and tstops == 1, tstarts)
 end
 
 function tests.multiloop_hotcounters()
+  teststart()
   local function f1(n, m) 
     local a = 0 
     for i=1,n do a = a + 1 end
@@ -150,27 +156,40 @@ function tests.multiloop_hotcounters()
     for i=1,m do b = b + 1 end
     return a, b
   end 
-  assert(get_hotcount(f1, countid_loop1) == lhot)
-  assert(get_hotcount(f1, countid_loop2) == lhot)
-  
+
   f1(1, -1)
-  assert(get_hotcount(f1, countid_loop1) == lhot-1)
-  assert(get_hotcount(f1, countid_loop2) == lhot)
-  assert(get_hotcount(f1, countid_loops) == 1)
+  assert(tstarts == 0, tstarts)
 
   f1(-1, 2)
-  assert(get_hotcount(f1, countid_loop1) == lhot-1)
-  assert(get_hotcount(f1, countid_loop2) == lhot-2)
-  assert(get_hotcount(f1, countid_loops) == 3)  
+  assert(tstarts == 0, tstarts)
+
+  -- Fist loop hot counter should be zero after this call
+  f1(lhot-1, -1)
+  assert(tstarts == 0, tstarts)
+
+  f1(3, 2)
+  assert(tstarts == 1 and tstops == 1, tstarts)
+  
+  -- Second loop hot counter should be zero after this call
+  f1(-1, lhot-4)
+  assert(tstarts == 1 and tstops == 1, tstarts)
+
+  -- Jit the second loop
+  f1(-1, 4)
+  assert(tstarts == 2 and tstops == 2, tstarts)
+
+  -- Both loops should be jit'ed not hot counting anymore
+  f1(lhot*3, lhot*3)
+  assert(tstarts == 2 and tstops == 2, tstarts)
 end
 
 function tests.func_backoff()
-  local function f2() return 2 end
+  teststart()
   local function f1(loopn)
     if loopn then
       local a = 0 
       -- Should abort root trace
-      for i=1,loopn do a = a + 1 end
+      nop()
       return a
     else
       return 1
@@ -178,169 +197,146 @@ function tests.func_backoff()
   end
   
   calln(f1, fhot)
-  assert(get_hotcount(f1, countid_func) == 0)
-  assert(get_hotcount(f1, countid_calls) == fhot)
-  
-  jitlog.start()
-  -- Trigger first trace attempt that aborts
-  f1(20)
+  assert(tstarts == 0, tstarts)
 
-  local result = parselog(jitlog.savetostring())
-  assert(#result.aborts == 1)
-  assert(result.aborts[1].callcount == fhot)
-  
-  -- Function hot count should of been reset with to the starting func_penalty hotvalue because the trace failed
-  assert(get_hotcount(f1, countid_func) == func_penalty)
-  -- a random offset is also added 
-  assert(get_hotcount(f1, countid_func) < func_penalty+16)
-  
-  jitlog.reset()
+  -- Trigger first trace attempt that aborts hitting a inner loop
+  f1(2)
+  assert(tstarts == 1 and taborts == 1, tstarts)
+
+  -- Decrement the hot counter to 0 or near it depending on random back off
   calln(f1, func_penalty)
-  -- Should trigger a second trace attempt that aborts
-  f1(20)
-  
-  -- Starting hotcount should of doubled with a small random backoff value added to it because the trace failed again.
-  assert(get_hotcount(f1, countid_func) >= func_penalty*2)
-  assert(get_hotcount(f1, countid_func) < func_penalty*2 + 16)
-  
-  result = parselog(jitlog.savetostring())
-  assert(#result.aborts == 1)
-  assert(result.aborts[1].callcount == fhot + func_penalty + 1)
-  assert(result.aborts[1].startpc == 0)
+  assert(tstarts == 1 and taborts == 1, tstarts)
+
+  -- Should trigger a second trace attempt that succeeds
+  calln(f1, random_backoff+1)
+  assert(tstarts == 2 and tstops == 1 and taborts == 1, tstarts)
 end
 
 function tests.loop_backoff()
-  local t = {a = 1, b = 2}
+  teststart()
   local function f1(n, abort) 
     local a = 0 
     for i=1,n do 
       a = a + 1
       if abort then
-        for _,_ in pairs(t) do end
+        nop()
       end
     end
     return a
   end 
-  
-  f1(lhot) 
-  assert(get_hotcount(f1, countid_loop2) == 0)
-  
-  jitlog.start()
+
+  f1(lhot)
+  assert(tstarts == 0, tstarts)
   -- Trigger first trace attempt that aborts
-  f1(1)
-  
-  local result = parselog(jitlog.savetostring())
-  assert(#result.aborts == 1)
-  assert(result.aborts[1].callcount == lhot)
-  -- Loop hot count should of been reset with to the starting loop_penalty hotvalue because the trace failed
-  assert(get_hotcount(f1, countid_loop2) == loop_penalty)
-  
-  f1(loop_penalty)
-  assert(get_hotcount(f1, countid_loop2) == 0)
-  
-  jitlog.reset()
-  -- Should trigger a second trace attempt that aborts
-  f1(1)
-  
-  local result = parselog(jitlog.savetostring())
-  assert(#result.aborts == 1)
-  assert(result.aborts[1].callcount == lhot + loop_penalty + 1)
-  -- Starting hotcount should of doubled with a small random backoff value added to it because the trace failed again.
-  assert(get_hotcount(f1, countid_loop2) >= loop_penalty*2)
-  assert(get_hotcount(f1, countid_loop2) < loop_penalty*2 + 16)
+  f1(3, true)
+  assert(tstarts == 1 and taborts == 1, tstarts)
+
+  -- Decrement the hot counter to 0 or near it depending on random back off
+  f1(loop_penalty-3)
+  assert(tstarts == 1 and taborts == 1, tstarts)
+
+  -- Trigger the next trace attempt that should succeed
+  f1(16)
+  assert(tstarts == 2 and tstops == 1 and taborts == 1, tstarts)
 end
 
 function tests.loop_blacklist()
-  local function f1(n) 
+  teststart()
+  local function f1(n, abort) 
     local a = 0 
+    local prev_abort = taborts
     for i=1,n do 
       a = a + 1
+      if abort then
+        -- Force an abort from calling a blacklisted function
+        nop()
+        if taborts ~= prev_abort then
+          return i
+        end
+      end
     end
     return a
   end
-  
-  jitlog.start()
+
   f1(lhot)
-  assert(get_hotcount(f1, countid_loop1) == 0)
-  f1(1)
-  assert(get_hotcount(f1, countid_loop1) == loop_penalty)
+  assert(tstarts == 0)
 
-  local prev = get_hotcount(f1, countid_loop1) 
+  f1(1, true)
+  assert(tstarts == 1 and taborts == 1)
+  
+  local count = loop_penalty
+  local rand_total = 0
   for i=1, maxattemps_loop-2 do
-    local count = get_hotcount(f1, countid_loop1)
-    prev = count
-    f1(count)
-    assert(get_hotcount(f1, countid_loop1) == 0)
+    f1(count-1)
+    assert(tstarts == i and taborts == i, tstarts .. i)
+
     -- Trigger another trace abort
-    f1(1)
-    local newcount = get_hotcount(f1, countid_loop1)
-    assert(newcount < countmax_loop)
-    -- New starting count should be double the last + random(16)
-    assert(newcount >= prev*2)
-    assert(newcount <= (prev*2) + 16)
+    f1(rand_total + 2, true)
+    assert(tstarts == i+1 and taborts == i+1, taborts .. (i+1))
+
+    count = count * 2
+    -- The random offset is added after the current count is doubled from an abort
+    if rand_total == 0 then
+      rand_total = random_backoff
+    else
+      rand_total = rand_total * 2 + random_backoff
+    end
   end
-
-  f1(get_hotcount(f1, countid_loop1))
-  assert(get_hotcount(f1, countid_loop1) == 0)
-
-  f1(1)
-  -- Last abort should blacklist the function the count will be reset to its default starting value
-  assert(get_hotcount(f1, countid_loop1) == lhot)
-
-  local result = parselog(jitlog.savetostring())
-  assert(#result.aborts == maxattemps_loop, #result.aborts)
-  assert(#result.proto_blacklist == 1)
-  assert(result.proto_blacklist[1].bcindex ~= 0)
-  assert(result.proto_blacklist[1].eventid == result.aborts[maxattemps_loop].eventid-1)
-  assert(result.proto_blacklist[1].proto == result.aborts[maxattemps_loop].startpt)
+  assert(tstarts == maxattemps_loop-1 and taborts == maxattemps_loop-1)
+  
+  -- Last abort should blacklist the function
+  f1(count + rand_total, true)
+  assert(tstarts == maxattemps_loop and taborts == maxattemps_loop)
+  
+  f1(0xffff * 2)
+  assert(tstarts == maxattemps_loop and taborts == maxattemps_loop)
 end
 
 function tests.func_blacklist()
-  local function f1(loopn)
-    if loopn then
+  teststart()
+  local t = {a = 1, b = 2}
+  local function f1(abort)
+    if abort then
       local a = 0 
       -- Should abort root trace
-      for i=1,loopn do a = a + 1 end
+      nop()
       return a
     else
       return 1
     end
   end
   
-  jitlog.start()
   calln(f1, fhot)
-  assert(get_hotcount(f1, countid_func) == 0)
-  f1(3)
-  assert(get_hotcount(f1, countid_func) == func_penalty)
+  assert(tstarts == 0)
 
-  local prev = get_hotcount(f1, countid_func) 
+  -- Trigger first abort
+  f1(true)
+  assert(tstarts == 1 and taborts == 1)
+
+  local count = func_penalty
+  local rand_total = 0
   for i=1, maxattemps_func-2 do
-    local count = get_hotcount(f1, countid_func)
-    prev = count
     calln(f1, count)
-    assert(get_hotcount(f1, countid_func) == 0)
-    -- Trigger another trace abort
-    f1(3)
-    local newcount = get_hotcount(f1, countid_func)
-    assert(newcount < 50000)
-    -- New starting count should be double the last + random(16)
-    assert(newcount >= prev*2)
-    assert(newcount <= (prev*2) + 16)
+    assert(tstarts == i and taborts == i, tstarts .. i)
+
+    calln(f1, rand_total + 2, true)
+    
+    assert(tstarts == i+1 and taborts == i+1, (taborts .. (i+1)))
+    count = count * 2
+    -- The random offset is added after the current count is doubled from an abort
+    if rand_total == 0 then
+      rand_total = random_backoff
+    else
+      rand_total = rand_total * 2 + random_backoff
+    end
   end
 
   calln(f1, get_hotcount(f1, countid_func))
-  assert(get_hotcount(f1, countid_func) == 0)
+  assert(tstarts == maxattemps_func-1 and taborts == maxattemps_func-1)
 
-  f1(3)
-  -- Last abort should blacklist the function the count will be reset to its default starting value
-  assert(get_hotcount(f1, countid_func) == fhot)
-
-  local result = parselog(jitlog.savetostring())
-  assert(#result.aborts == maxattemps_func, #result.aborts)
-  assert(#result.proto_blacklist == 1)
-  assert(result.proto_blacklist[1].bcindex == 0)
-  assert(result.proto_blacklist[1].eventid == result.aborts[maxattemps_func].eventid-1)
-  assert(result.proto_blacklist[1].proto == result.aborts[maxattemps_func].startpt)
+  -- Trigger the last abort which should get the function blacklisted
+  f1(16)
+  assert(tstarts == maxattemps_func and taborts == maxattemps_func)
 end
 
 local failed = false
